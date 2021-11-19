be653fc1da8fe50a8dbd0d3109c80cab9c3b44fc
germansanz93 <germansanz93@gmail.com>
2021-11-19 11:45:47 -0300

    added content to german1, German2 and a println in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
   public static void main(String args[]){
     System.out.println("Hello World!");
+    System.out.println("Mi primer HolaMundo en java, ya puedo poner en mi CV que soy java developer!!")
   }
 }
--- german1.md
+++ german1.md
@@ -1,2 +1,4 @@
 ## Este es un MarkDown de prueba##
 Me llamo German y tengo 27 anios (y si, no tengo enie).
+
+Por aca no voy a poner Lorem
